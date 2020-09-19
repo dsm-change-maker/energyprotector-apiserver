0eee01c73a550678fe67cd82363c167efca8ba10
조호원 <hwc9169@gmail.com>
2020-09-19 13:45:25 +0900

    configuration file

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///C:\\Users\\user\\Desktop\\energyprotector\\energyprotector-apiserver\\energyprotector.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////home/skyhelper/repository/energyprotector/energyprotector-apiserver/energyprotector.db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 JWT_SECRET_KEY = 'salty salt'
